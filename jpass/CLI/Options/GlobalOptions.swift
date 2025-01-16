4735f95351830ade553cbd88b3c25f46907032f8
Corey Oliphant <corey.oliphant@icloud.com>
2025-01-16 09:29:16 -0600

    Added option to set noCache via environment variable

--- jpass/CLI/Options/GlobalOptions.swift
+++ jpass/CLI/Options/GlobalOptions.swift
@@ -13,6 +13,7 @@ struct GlobalOptions: ParsableCommand {
     static let userKey = "JPASS_USER"
     static let serverKey = "JPASS_SERVER"
     static let clientIdKey = "JPASS_CLIENT_ID"
+    static let noCacheKey = "JPASS_NO_CACHE"
     
     @Option(name: .shortAndLong, help: "The Jamf Pro server URL and port (if not standard). If no port is defined, the default port 443 will be used for Jamf Cloud instances and 8443 for everything else. Can be set using the environment variable \(serverKey).")
     var server: String?
@@ -23,7 +24,7 @@ struct GlobalOptions: ParsableCommand {
     @Option(name: .long, help: "The Jamf Pro API client used for authentication. Can be set using the environment variable \(clientIdKey).")
     var clientId: String?
     
-    @Flag(name: .long, help: "Disables retrieval and caching of credentials in local keychain.")
+    @Flag(name: .long, help: "Disables retrieval and caching of credentials in local keychain. Can be set using the environment variable \(noCacheKey).")
     var noCache = false
     
     @Flag(name: .shortAndLong, help: "Enable verbose logging.")
@@ -44,16 +45,17 @@ struct GlobalOptions: ParsableCommand {
     }
     
     mutating func validate() throws {
+        // Retrieve any set environment variables
         if server == nil {
-            if let envVar = ProcessInfo.processInfo.environment[GlobalOptions.serverKey] {
+            if let envVar = ProcessInfo.processInfo.environment[Self.serverKey] {
                 server = envVar
             } else {
                 throw ValidationError("Server address must be provided.")
             }
         }
         
-        let userEnvVar = ProcessInfo.processInfo.environment[GlobalOptions.userKey]
-        let clientIdEnvVar = ProcessInfo.processInfo.environment[GlobalOptions.clientIdKey]
+        let userEnvVar = ProcessInfo.processInfo.environment[Self.userKey]
+        let clientIdEnvVar = ProcessInfo.processInfo.environment[Self.clientIdKey]
         
         if (user == nil && userEnvVar == nil) && (clientId == nil && clientIdEnvVar == nil) {
             throw ValidationError("A Jamf user or API client id must be provided.")
@@ -65,6 +67,10 @@ struct GlobalOptions: ParsableCommand {
             } else if let clientIdEnvVar, clientId == nil {
                 clientId = clientIdEnvVar
             }
+        }
+        
+        if let _ = ProcessInfo.processInfo.environment[Self.noCacheKey] {
+            noCache = true
         }
         
         GlobalSettings.shared.verbose = verbose
